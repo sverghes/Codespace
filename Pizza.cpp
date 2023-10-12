42b9315dc1a4b601083919861c1760e1bf02a329
sverghes <94053918+sverghes@users.noreply.github.com>
2023-10-12 14:48:03 +0100

    Create Pizza.cpp

---
+++ Pizza.cpp
@@ -0,0 +1,31 @@
+// Programming Challenge 22, Pizza Slices 
+
+//  This program calculates the number of slices in a pizza.
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;            
+
+int main()
+{
+    const double PI = 3.14,
+                 SLICE_SIZE = 14.125;   // Sq. inches in each slice
+
+    double pizzaDiameter,       // Diameter of the pizza (in inches)
+           pizzaRadius,         // Radius of the pizza (in inches)
+           pizzaArea,           // Area of the pizza (in sq. inches)
+           slicesPerPizza;      // Number of slice to cut pizza into
+
+    
+    // Display results
+    cout << "Fill in your code";  // Display result to nearest whole number
+    cout << "Cut this pizza into " << "Fill in your variable name" << " slices." << endl;
+
+    return 0;
+}
+
+/* SAMPLE RUN RESULTS
+
+Enter the pizza diameter (in inches): 14
+Cut this pizza into 11 slices.
+*/
